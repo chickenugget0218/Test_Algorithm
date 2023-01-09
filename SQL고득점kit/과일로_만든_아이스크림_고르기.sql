2208d8f27f68d4dab75a8cacfbfaeec4ee51665c
chickenugget0218 <chickenugget0218@gmail.com>
2023-01-09 17:47:14 +0900

    230109

---
+++ SQL고득점kit/과일로_만든_아이스크림_고르기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT f.flavor
+from first_half as f
+left join icecream_info as i
+on f.flavor = i.flavor
+where f.total_order > 3000 and i.ingredient_type = 'fruit_based'
+group by flavor
+order by f.total_order desc;
